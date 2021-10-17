ab78f85d107a255c584ae842d3e87480663b5f99
luCyfahh <filipe.venancio@fixus.co.ao>
2021-10-17 05:33:41 +0100

    connectionstring
    Added the string to connect to the DB

--- app.js
+++ app.js
@@ -10,7 +10,13 @@ app.use(express.static('public'));
 app.set('view engine', 'ejs');
 
 // database connection
-const dbURI = '';
+const dbURI = 'ConnectionString connectionString = new ConnectionString("mongodb+srv://sa:sapwd1234@smartagriculture.cp6lg.mongodb.net/thos?retryWrites=true&w=majority");
+                MongoClientSettings settings = MongoClientSettings.builder()
+                  .applyConnectionString(connectionString)
+                  .build();
+                MongoClient mongoClient = MongoClients.create(settings);
+                MongoDatabase database = mongoClient.getDatabase("test");';
+
 mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex:true })
   .then((result) => app.listen(3000))
   .catch((err) => console.log(err));
